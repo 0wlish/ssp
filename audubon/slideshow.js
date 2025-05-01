1ff2c4f05443b7346a3dd25c949e88b88a46136f
0wlish <tortugavpn@gmail.com>
2025-05-01 10:52:06 -0400

    added week 5 list, working on audubon slideshow

---
+++ audubon/slideshow.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slides");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides.item(i).style.display = "none";
+    }
+    slides.item(slideIndex).style.display = "block";
+}
