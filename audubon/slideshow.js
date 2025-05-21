31dd173adb10081cb3160f5e1bcb7d5e445f7d2f
0wlish <tortugavpn@gmail.com>
2025-05-21 09:10:20 -0400

    making second slideshow

--- audubon/slideshow.js
+++ audubon/slideshow.js
@@ -1,5 +1,7 @@
 let slideIndex = 0;
+let slideIndex2 = 0;
 showSlides(slideIndex);
+showSlides2(slideIndex2);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -16,4 +18,21 @@ function showSlides(n) {
         slides.item(i).style.display = "none";
     }
     slides.item(slideIndex).style.display = "block";
+}
+//can we pretend that this code is well-written? that would be nice
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+}
+function showSlides2(n) {
+    let slides2 = document.getElementsByClassName("slides2");
+    if (n >= slides2.length) {
+        slideIndex2 = 0;
+    }
+    else if (n < 0) {
+        slideIndex2 = slides2.length - 1;
+    }
+    for (let i = 0; i < slides2.length; i++) {
+        slides2.item(i).style.display = "none";
+    }
+    slides2.item(slideIndex2).style.display = "block";
 }
